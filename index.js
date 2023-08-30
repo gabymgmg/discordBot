07d06afc78f7433778ce9a0a59f3d44c5d671774
Motogabi <gabymgmg98@gmail.com>
2023-08-30 19:07:24 -0300

    more event handling

--- index.js
+++ index.js
@@ -37,5 +37,20 @@ for (const file of commandFiles) {
     }
 }
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+// Loading the eventFiles (event handling)
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    }
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
 // Log in to Discord with the bot's token
 client.login(process.env.TOKEN);
