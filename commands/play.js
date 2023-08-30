c1d419d081066239c2194b519e3949fbf9c97ca1
Motogabi <gabymgmg98@gmail.com>
2023-08-30 19:56:23 -0300

    string option added, nodemon addition

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const wait = require('node:timers/promises').setTimeout;
 const { EmbedBuilder } = require('discord.js');
 const ytSearch = require('yt-search');
 
@@ -7,9 +8,16 @@ const ytSearch = require('yt-search');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
-        .setDescription('Plays a song from Youtube'),
+        .setDescription('Plays a song from Youtube')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The name/link of the song')
+                .setRequired(true)),
     execute: async (interaction) => {
-        await interaction.reply('Reproduciendo');
+        await interaction.deferReply();
+        await wait(4000);
+        await interaction.followUp('Embed message');
+
     },
 };
 
