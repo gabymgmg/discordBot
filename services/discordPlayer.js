e019f9dcdacadaf37e9e3566ce64eaece7c9f389
Alejandro F <progressandro@gmail.com>
2023-08-30 21:19:38 -0300

    Added discord

---
+++ services/discordPlayer.js
@@ -0,0 +1,12 @@
+const youtubeSearch = require('yt-search');
+async function searchByName(songName, resultCount = 1) {
+    if (resultCount < 1) {
+        throw new Error('Result count must be greater than 0');
+    }
+    const result = await youtubeSearch(songName);
+    return resultCount ? result.videos.slice(0, resultCount) : result.videos[0];
+}
+
+module.exports = {
+    searchByName,
+};
