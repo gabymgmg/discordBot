d57cff6ca1ea9795cdf98fd65d951d579d57734c
Motogabi <gabymgmg98@gmail.com>
2023-08-31 14:39:40 -0300

    discordPlayer function modification - it can now get a url or text

--- services/discordPlayer.js
+++ services/discordPlayer.js
@@ -1,12 +1,21 @@
 const { EmbedBuilder } = require('@discordjs/builders');
 const youtubeSearch = require('yt-search');
+const { isYoutubeUrl } = require('../lib/helpers');
+const { getVideoID } = require('ytdl-core');
+
 async function youtubeSearchByText(songName, resultCount = 1) {
+    // check if it's a songName or link
+    const isUrl = isYoutubeUrl(songName);
+    if (!isUrl) {
+        songName = getVideoID(songName);
+    }
     if (resultCount < 1) {
         throw new Error('Result count must be greater than 0');
     }
     const result = await youtubeSearch(songName);
     return resultCount > 1 ? result.videos.slice(0, resultCount) : result.videos[0];
 }
+
 
 function convertToEmbeds(songs, user) {
 
