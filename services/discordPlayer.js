004401205379acadedb4d1370c86021dccebe871
Alejandro F <progressandro@gmail.com>
2023-08-30 22:05:13 -0300

    convertToEmbeds

--- services/discordPlayer.js
+++ services/discordPlayer.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require('@discordjs/builders');
 const youtubeSearch = require('yt-search');
 async function youtubeSearchByText(songName, resultCount = 1) {
     if (resultCount < 1) {
@@ -7,6 +8,21 @@ async function youtubeSearchByText(songName, resultCount = 1) {
     return resultCount ? result.videos.slice(0, resultCount) : result.videos[0];
 }
 
+function convertToEmbeds(songs, user) {
+    return songs.map((song) => {
+        const embed = new EmbedBuilder();
+        embed.setFields(
+            { name: 'title', value: song.title },
+            { name: 'url', value: song.url },
+            { name: 'thumbnail', value: song.thumbnail },
+            { name: 'description', value: song.description },
+        );
+        if (user) {
+            embed.setFooter({ text: user.username, iconURL: user.avatarURL });
+        }
+    });
+}
 module.exports = {
     youtubeSearchByText,
+    convertToEmbeds,
 };
